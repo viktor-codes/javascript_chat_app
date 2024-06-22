2d1f737d72106fea4efcd66a38dda75e3730eac9
Viktor Rura <ruravictor@gmail.com>
2024-06-22 11:18:51 +0100

    Added event listeners for typing and for connecting and disconnecting actions.

--- server/index.js
+++ server/index.js
@@ -29,8 +29,32 @@ const io = new Server(expressServer, {
 io.on("connection", (socket) => {
     console.log(`User ${socket.id} connected`);
 
+    // Upon connection - only to user
+
+    socket.emit("message", "Welcome to the chat room!");
+
+    // Upon connection - all others
+    socket.broadcast.emit(
+        "message",
+        `User ${socket.id.substring(0, 5)} connected`
+    );
+
+    // Listen for activity
     socket.on("message", (data) => {
         console.log(data);
         io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
+    });
+
+    // When user disconnects - to all others
+    socket.on("disconnect", () => {
+        socket.broadcast.emit(
+            "message",
+            `User ${socket.id.substring(0, 5)} disconnected`
+        );
+    });
+
+    // Listen for activity
+    socket.on("activity", (name) => {
+        socket.broadcast.emit("activity", name);
     });
 });
--- server/public/app.js
+++ server/public/app.js
@@ -1,20 +1,32 @@
 const socket = io("ws://localhost:3500");
 
+const activity = document.querySelector(".activity");
+const msgInput = document.querySelector("input");
+
 function sendMessage(e) {
     e.preventDefault();
-    const input = document.querySelector("input");
-    if (input.value) {
-        socket.send("message", input.value);
-        input.value = "";
+
+    if (msgInput.value) {
+        socket.emit("message", msgInput.value);
+        msgInput.value = "";
     }
-    input.focus();
+    msgInput.focus();
 }
 
 document.querySelector("form").addEventListener("submit", sendMessage);
 
 // Listen for messages
 socket.on("message", (data) => {
+    activity.textContent = "";
     const li = document.createElement("li");
     li.textContent = data;
     document.querySelector("ul").appendChild(li);
+});
+
+msgInput.addEventListener("keypress", () => {
+    socket.emit("activity", socket.id.substring(0, 5));
+});
+
+socket.on("activity", (name) => {
+    activity.textContent = `${name} is typing...`;
 });
