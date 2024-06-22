e6021f37613d58e07f6cf90c29f4c87a01647fe2
Viktor Rura <ruravictor@gmail.com>
2024-06-22 11:21:25 +0100

    Fixed type listener by adding the timer.

--- server/public/app.js
+++ server/public/app.js
@@ -27,6 +27,13 @@ msgInput.addEventListener("keypress", () => {
     socket.emit("activity", socket.id.substring(0, 5));
 });
 
+let activityTimer
 socket.on("activity", (name) => {
     activity.textContent = `${name} is typing...`;
+
+    //Clear after 3 seconds
+    clearTimeout(activityTimer);
+    activityTimer = setTimeout(() => {
+        activity.textContent = "";
+    }, 3000);
 });
