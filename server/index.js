f79a07293a8f25109f88a5e9986dc2b6a15e6a0a
Viktor Rura <ruravictor@gmail.com>
2024-06-22 10:59:10 +0100

    Installed express js, created public folder and moved there files from app.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
-import { createServer } from "http";
+import express from "express";
 import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const httpServer = createServer();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const io = new Server(httpServer, {
+
+const PORT = process.env.PORT || 3500;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const expressServer = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+const io = new Server(expressServer, {
     cors: {
         origin:
             process.env.NODE_ENV === "production"
@@ -16,9 +30,7 @@ io.on("connection", (socket) => {
     console.log(`User ${socket.id} connected`);
 
     socket.on("message", (data) => {
-        console.log(data)
-        io.emit("message", `${socket.id.substring(0,5)}: ${data}`);
+        console.log(data);
+        io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
     });
 });
-
-httpServer.listen(3500, () => console.log("Server is running on port 3500"));
