a637f1bd1b925b45bc4b4ecba6bafa2a5e8b2338
Viktor Rura <ruravictor@gmail.com>
2024-06-22 15:20:30 +0100

    Adde functionality to listen to the messages inside the app.js file.

--- server/public/app.js
+++ server/public/app.js
@@ -45,8 +45,17 @@ msgInput.addEventListener("keypress", () => {
 // Listen for messages
 socket.on("message", (data) => {
     activity.textContent = "";
+    const {name, text, time } = data;
     const li = document.createElement("li");
-    li.textContent = data;
+    li.className = "post";
+    if (name === nameInput.value) li.classList.add("me") = "post post--left"
+    if (name !== nameInput.value && name !== "Admin") li.classList.add("post post--right")
+    if (name !== "Admin") li.innerHtml = `<div class="post__header ${name === nameInput.value ? "post__header--user" : "post__header--reply"}">
+    <span class="post__header--name">${name}</span>
+    <span class="post__header--time">${time}</span>
+    </div>
+    <div class="post__text">${text}</div>`
+    
     document.querySelector("ul").appendChild(li);
 });
 
